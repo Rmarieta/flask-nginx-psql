8c10d48bb06785b63d6cc09e9697b8058782f678
Rmarieta <raphael.marietan@hotmail.com>
2024-03-04 09:22:46 +0100

    Removing FlaskGroup.cli + adding run.sh

--- flask/application.py
+++ flask/application.py
@@ -3,16 +3,8 @@ monkey.patch_all()
 
 from flask import Flask, jsonify
 from app import create_app, db, socketio
-from flask.cli import FlaskGroup
 
 application = create_app()
-cli = FlaskGroup(application)
-
-# define that to create the tables having access to the app context from the container shell
-@cli.command('create_all')
-def create_all():
-    with application.app_context():
-        db.create_all()
 
 import os
 HOSTNAME = os.uname().nodename
@@ -22,5 +14,4 @@ def get_hostname():
     return jsonify({'hostname': HOSTNAME})
 
 if __name__ == "__main__":
-    cli()
-    # socketio.run(application, debug=True, host='0.0.0.0')
+    socketio.run(application, debug=True, host='0.0.0.0')
